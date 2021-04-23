d99880b8245b60858b733b29351eabc84696bcb9
AikChun <iam@simaikchun.com>
2021-04-23 12:15:13 +0800

    Added example of declare interfaces with a function as a property in it.

--- 005-interfaces/interfaces.ts
+++ 005-interfaces/interfaces.ts
@@ -2,12 +2,16 @@ const oldCivic = {
   name: 'civic',
   isBroken: true,
   year: 2000,
+  summary(): string {
+    return `Name; ${this.name}`;
+  },
 };
 
 interface Vehicle {
   name: string;
   isBroken: boolean;
   year: number;
+  summary(): string;
 }
 
 const printVehicle = (vehicle: Vehicle): void => {
