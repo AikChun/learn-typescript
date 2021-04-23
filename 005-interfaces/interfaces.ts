0dfa46a4d46104bb7a77de956854c6d58dbe13fb
AikChun <iam@simaikchun.com>
2021-04-23 12:29:22 +0800

    Added an example by using aDrink to show that an object just has to satisfy the property types declared on the interface to be considered as the same type, additional properties does not disqualify the object.

--- 005-interfaces/interfaces.ts
+++ 005-interfaces/interfaces.ts
@@ -11,8 +11,17 @@ const oldCivic = {
   },
 };
 
+const aDrink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar.`;
+  },
+};
 const printSummary = (item: Reportable): void => {
   console.log(item.summary());
 };
 
-printVehicle(oldCivic);
+printSummary(oldCivic);
+printSummary(aDrink);
