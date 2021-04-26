24eadcd4b6d379e1f9237a557effa381395dbadc
AikChun <iam@simaikchun.com>
2021-04-26 16:19:59 +0800

    Changed CustomMap constructor to take in an element id to increase its usabiliy.

--- 007-maps/src/CustomMap.ts
+++ 007-maps/src/CustomMap.ts
@@ -1,7 +1,7 @@
 export class CustomMap {
   private googleMap: google.maps.Map;
-  constructor() {
-    this.googleMap = new google.maps.Map(document.getElementById('map'), {
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
       zoom: 1,
       center: { lat: 0, lng: 0 },
     });
