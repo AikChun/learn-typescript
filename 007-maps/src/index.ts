2548ed472c51e2391805a038b29fa829a4f4b99e
AikChun <iam@simaikchun.com>
2021-04-26 16:40:56 +0800

    Added implementation to addCompanyMarker method in CustomMap class.

--- 007-maps/src/index.ts
+++ 007-maps/src/index.ts
@@ -1,6 +1,9 @@
 import { User } from './User';
 import { CustomMap } from './CustomMap';
+import { Company } from './Company';
 
 const map = new CustomMap('map');
 const user = new User();
+const company = new Company();
 map.addUserMarker(user);
+map.addCompanyMarker(company);
