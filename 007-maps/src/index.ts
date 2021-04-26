070d4f8ff11a426e6898e9879584e91f96a552cf
AikChun <iam@simaikchun.com>
2021-04-26 17:46:42 +0800

    Replaced addUserMarker and addCompanyMarker to a generic function that takes in a Mappable interface. And added InfoWindow to the markers.

--- 007-maps/src/index.ts
+++ 007-maps/src/index.ts
@@ -5,5 +5,6 @@ import { Company } from './Company';
 const map = new CustomMap('map');
 const user = new User();
 const company = new Company();
-map.addUserMarker(user);
-map.addCompanyMarker(company);
+
+map.addMarker(user);
+map.addMarker(company);
