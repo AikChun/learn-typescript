f69bf6dfe44b9a5a6f589e4fca3ba51d0b30af96
AikChun <iam@simaikchun.com>
2021-04-26 12:56:58 +0800

    Added company class, googlemapsapi script tag and googlemaps type definition file.

--- 007-maps/src/index.ts
+++ 007-maps/src/index.ts
@@ -1,1 +1,9 @@
+import { Company } from './Company';
+import { User } from './User';
+
 console.log('Hi there!');
+const user = new User();
+console.log(user);
+
+const company = new Company();
+console.log(company);
