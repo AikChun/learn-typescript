ff649b9db4c834d761385866c0b6c81de09dbdc2
AikChun <iam@simaikchun.com>
2021-04-27 23:15:08 +0800

    Implemented bubble sort algorithm.

--- 008-sort/src/index.ts
+++ 008-sort/src/index.ts
@@ -1,3 +1,21 @@
-console.log('Hello World!');
+class Sorter {
+  constructor(public collection: number[]) {}
+  sort(): void {
+    const { length } = this.collection;
+    
+    for (let i=0;i<length;i++) {
+      for (let j=0;j<length-i-1; j++) {
+        if (this.collection[j] > this.collection[j+1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j+1];
+          this.collection[j+1] = leftHand;
+        }
+      }
 
-console.log('Goodbye world!');
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -1, 0]);
+sorter.sort();
+console.log(sorter.collection);
