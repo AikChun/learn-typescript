6ab9e1ae9c3a2f1f2954f9e3f0cbd5610bc1fc9e
AikChun <iam@simaikchun.com>
2021-04-27 08:39:47 +0800

    Added color field on Mappable interface. Changed implementation on User and Company class to implement Mappable.

--- 007-maps/src/Company.ts
+++ 007-maps/src/Company.ts
@@ -1,12 +1,15 @@
 import faker from 'faker';
+import { Mappable } from './CustomMap';
 
-export class Company {
+export class Company implements Mappable {
   name: string;
   catchPhrase: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string;
+
   constructor() {
     this.name = faker.company.companyName();
     this.catchPhrase = faker.company.catchPhrase();
