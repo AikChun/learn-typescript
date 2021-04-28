ffec2588abbd222b7e14d4a42421246969ddd356
AikChun <iam@simaikchun.com>
2021-04-28 16:36:19 +0800

    Implemented a NumbersCollection and Added a Sorter class in its own file.

--- 008-sort/build/index.js
+++ 008-sort/build/index.js
@@ -1,22 +1,7 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
-var sorter = new Sorter([10, 3, -1, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+var NumbersCollection_1 = require("./NumbersCollection");
+var Sorter_1 = require("./Sorter");
+var sorter = new Sorter_1.Sorter(new NumbersCollection_1.NumbersCollection([10, 3, -1, 0]));
 sorter.sort();
 console.log(sorter.collection);
