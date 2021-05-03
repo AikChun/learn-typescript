fde002fea33b0668f63df95f36cf25a0ea4490f6
AikChun <iam@simaikchun.com>
2021-05-03 12:42:54 +0800

    Added example on using Generic Constraints.

--- 010-generics/generics.ts
+++ 010-generics/generics.ts
@@ -46,3 +46,27 @@ function printAnything<T>(arr: T[]) {
 
 printAnything<string>(['x', 'y', 'z']);
 printAnything(['x', 'y', 'z']);
+
+// generic constraints
+
+class Car {
+  print() {
+    console.log('I am a car');
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a house');
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
