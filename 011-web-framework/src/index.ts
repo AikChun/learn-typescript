5a7079dd63a0b924baa62c42e51bbe124ef46c7f
AikChun <iam@simaikchun.com>
2021-05-05 17:38:15 +0800

    Added implementation to trigger callback when the Collection successfully resolves the axios request.

--- 011-web-framework/src/index.ts
+++ 011-web-framework/src/index.ts
@@ -1,6 +1,9 @@
 import { Collection } from './models/Collection';
 
 const collection = new Collection('http://localhost:3000/users');
+collection.on('change', () => {
+  console.log('collection changed!');
+});
 
 collection.fetch();
 
