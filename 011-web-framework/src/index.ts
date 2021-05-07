f1945d91013b0ce66c9b39a66b7301e0cc36121d
AikChun <iam@simaikchun.com>
2021-05-07 16:32:26 +0800

    Implement UserList and added an example on how to render a list of models.

--- 011-web-framework/src/index.ts
+++ 011-web-framework/src/index.ts
@@ -1,14 +1,16 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
 
-const user = User.buildUser({ name: 'NAME', age: 22 });
+const collection = User.buildUserCollection();
 
-const root = document.getElementById('root');
+collection.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    new UserList(root, collection).render();
+  } else {
+    throw new Error("can't find root element");
+  }
+});
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw new Error('Root element not found.');
-}
+collection.fetch();
