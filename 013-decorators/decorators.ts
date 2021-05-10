b7ca04deca6bfbd14667c1c9cfb1ab7377c45885
AikChun <iam@simaikchun.com>
2021-05-10 12:22:03 +0800

    Added tsconfig to enable experimental decorators

--- 013-decorators/decorators.ts
+++ 013-decorators/decorators.ts
@@ -5,7 +5,15 @@ class Boat {
     return `This boat color is ${this.color}`;
   }
 
+  @testDecorator
   pilot(): void {
     console.log('swish');
   }
 }
+
+function testDecorator(target: any, key: string): void {
+  console.log(`Target: ${target}`);
+  console.log(`Key: ${key}`);
+}
+
+new Boat().pilot();
