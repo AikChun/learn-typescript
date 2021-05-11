79fc8faa49c76e1cbdde83a44f3e283c5819d69c
AikChun <iam@simaikchun.com>
2021-05-11 17:27:56 +0800

    Extended PropertyDescriptor to add validation on the type of function the decorator can be used on.

--- 012-typescript-express/server/src/controllers/decorators/routes.ts
+++ 012-typescript-express/server/src/controllers/decorators/routes.ts
@@ -1,11 +1,16 @@
 import 'reflect-metadata';
+import { RequestHandler, Router } from 'express';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
+
+interface RouteHandlerDescriptor {
+  value?: RequestHandler;
+}
 
 const createRoute = (method: string) => (path: string) => (
   target: any,
   key: string,
-  desc: PropertyDescriptor
+  desc: RouteHandlerDescriptor
 ) => {
   Reflect.defineMetadata(MetadataKeys.PATH, path, target, key);
   Reflect.defineMetadata(MetadataKeys.METHOD, method, target, key);
