e6315835d845e8e1f6fe1ba7702ec4db18f4eca9
AikChun <iam@simaikchun.com>
2021-05-12 07:08:28 +0800

    Implemented a counter and buttons to increment and decrement.

--- 012-typescript-express/rrts/src/index.tsx
+++ 012-typescript-express/rrts/src/index.tsx
@@ -4,12 +4,25 @@ import ReactDOM from 'react-dom';
 interface AppProps {
     color: string;
 }
-// class App extends React.Component<AppProps> {
-//     render() {
-//         return <div>{this.props.color}</div>
-//     }
-// }
+class App extends React.Component<AppProps> {
+    state = { counter: 0 }
+    onIncrement = (): void => {
+        this.setState({ counter: this.state.counter + 1 });
+    }
+    onDecrement = (): void => {
+        this.setState({ counter: this.state.counter -1 });
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.onIncrement}>Increment</button>
+                <button onClick={this.onDecrement}>Decrement</button>
+                {this.state.counter}
+            </div>
+        )
+    }
+}
 
-const App = (props: AppProps) => <div>{props.color}</div>
+// const App = (props: AppProps) => <div>{props.color}</div>
 
 ReactDOM.render(<App color="red"/>, document.querySelector('#root'));
