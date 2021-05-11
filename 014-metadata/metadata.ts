0565d521d830a31c43e2beba0bf9976131a48fc9
AikChun <iam@simaikchun.com>
2021-05-11 10:43:51 +0800

    Added example on how to use/add metadata on objects and its fields/properties.

---
+++ 014-metadata/metadata.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata';
+
+const plane = {
+  color: 'red',
+};
+
+Reflect.defineMetadata('note', 'hi there color', plane, 'color');
+
+const note = Reflect.getMetadata('note', plane, 'color');
+console.log(note);
+
+// Reflect.defineMetadata('note', 'hi there', plane);
+
+// const note = Reflect.getMetadata('note', plane);
+
+// console.log(note);
