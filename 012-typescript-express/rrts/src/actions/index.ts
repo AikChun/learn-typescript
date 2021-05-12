cc85454ac0ae6aa502027f608a68a34f13487d1e
AikChun <iam@simaikchun.com>
2021-05-12 13:16:27 +0800

    Moved todos related action creators into its own file.

--- 012-typescript-express/rrts/src/actions/index.ts
+++ 012-typescript-express/rrts/src/actions/index.ts
@@ -1,26 +1,2 @@
-import axios from 'axios';
-import { Dispatch } from 'redux';
-import { ActionTypes } from './types';
-
-const url = 'https://jsonplaceholder.typicode.com/todos';
-
-export interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-}
-
-export interface FetchTodosAction {
-  type: ActionTypes;
-  payload: Todo[];
-}
-
-export const fetchTodos =
-  () => async (dispatch: Dispatch<FetchTodosAction>) => {
-    const response = await axios.get<Todo[]>(url);
-
-    dispatch({
-      type: ActionTypes.fetchTodos,
-      payload: response.data,
-    });
-  };
+export * from './todos';
+export * from './types';
