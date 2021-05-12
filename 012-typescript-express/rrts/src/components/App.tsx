e434ea4cd427aade93a17a42a7823c8a717d13ca
AikChun <iam@simaikchun.com>
2021-05-12 12:19:46 +0800

    Wired up reducer to the App component with a lot type annotations.

--- 012-typescript-express/rrts/src/components/App.tsx
+++ 012-typescript-express/rrts/src/components/App.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos} from '../actions';
+import { StoreState } from '../reducers/index';
 
-export const App = ():JSX.Element => {
-    return <div>Hi there</div>
+interface AppProps {
+    todos: Todo[];
+    fetchTodos(): any;
+
 }
+
+export const _App = (props: AppProps):JSX.Element => {
+    const fetchAllTodos = ():void => {
+        props.fetchTodos();
+    };
+
+    const renderList = (): JSX.Element[]  =>  props.todos.map((todo: Todo) => <div key={todo.id}>{todo.title}</div>)
+    return (<div>
+        <button onClick={fetchAllTodos}>Fetch</button>
+        {renderList()}
+
+    </div>)
+}
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[]} => {
+    return { todos };
+
+}
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
